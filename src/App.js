9f0da6cace48acab54cc91aa25118771b6d92494
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2022-01-12 05:03:54 +0530

    Expenses componenet added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from './Components/Expenses'
+
 
 function App() {
+  const expenses = [
+    { 
+      id: 'a1',
+      title:  'mobile1',
+      amount: 2001,
+      date: new Date(2020,1,10),
+      },
+    { 
+      id: 'a2',
+      title:  'mobile2',
+      amount: 2002,
+      date: new Date(2020,2,10),
+      },
+    { 
+      id: 'a3',
+      title:  'mobile3',
+      amount: 2003,
+      date: new Date(2020,3,10),
+      },
+    { 
+      id: 'a4',
+      title:  'mobile4',
+      amount: 2004,
+      date: new Date(2020,4,10),
+      },
+
+
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>started </h1>
+      <Expenses items={expenses}/>
     </div>
   );
 }
