99d0c46fed1429c0ca1b0aef7f22d1b6e6468d7f
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2022-01-15 01:15:04 +0530

    dynamically rendering Expense item component hardcoded removed

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,38 +1,31 @@
-
-import ExpenseItem from './ExpenseItem';
-import ExpensesFilter from './ExpensesFilter';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
-import './Expenses.css';
-
+import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const yearChangeHandler = (selectedYear) => {
+    console.log("in expenses");
+    setFilteredYear(selectedYear);
+  };
   return (
     <div>
-      <ExpensesFilter/>
       <Card className="expenses">
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesFilter reset={filteredYear} onYearChange={yearChangeHandler} />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
